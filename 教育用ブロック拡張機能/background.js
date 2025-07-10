0f8c6c44e356470cf9991035cdf90aa413b09349
kcjsa <kojimakcjsa@gmail.com>
2025-07-10 18:26:04 +0900

    Add files via upload

---
+++ 教育用ブロック拡張機能/background.js
@@ -0,0 +1,3 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("教育用ブロック拡張機能インストール完了");
+});
---
+++ 教育用ブロック拡張機能/src/firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7d7RqunxZwxyCi5Jm4Cl9yUQNEsPhJFA",
+  authDomain: "education-e3dae.firebaseapp.com",
+  databaseURL: "https://education-e3dae-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "education-e3dae",
+  storageBucket: "education-e3dae.firebasestorage.app",
+  messagingSenderId: "812341725803",
+  appId: "1:812341725803:web:bb576c7fa472e32d113f2e",
+  measurementId: "G-9MLZ2VGJGC"
+};
+// この拡張機能の提供先組織名を設定（学校や会社名など）
+export const ORG_ID = "";
